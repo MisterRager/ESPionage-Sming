5edc97fa24a76859a0a38cab8fa9ec83c4660298
Alan Rager <alan.rager@gmail.com>
2016-01-15 21:19:54 -0800

    trimming some stuff out, forgot some bits from last commit

--- app/application.cpp
+++ app/application.cpp
@@ -2,129 +2,42 @@
 #include <SmingCore/SmingCore.h>
 
 #include "apa102.cpp"
+#include "tpm2_net.c"
+#include "application.hpp"
 
-struct Color {
-  uint8_t brightness;
-  uint8_t red;
-  uint8_t green;
-  uint8_t blue;
-};
-
-Color currentColor = {
-  0xFF,
-  0,
-  99,
-  10
-};
-
-size_t LED_LENGTH = 200;
-HttpServer server;
-
-void updateLights() {
-  showColor(
-    LED_LENGTH,
-    currentColor.brightness,
-    currentColor.blue,
-    currentColor.green,
-    currentColor.red
-  );
-}
-
-void onFile(HttpRequest &request, HttpResponse &response) {
-  String file = request.getPath();
-  if (file[0] == '/')
-    file = file.substring(1);
-
-  if (file[0] == '.')
-    response.forbidden();
-  else
-  {
-    response.setCache(86400, true); // It's important to use cache for better performance.
-    response.sendFile(file);
-  }
-}
-
-void ajaxGetColor(HttpRequest &request, HttpResponse &response) {
-  JsonObjectStream* stream = new JsonObjectStream();
-  JsonObject& json = stream->getRoot();
-
-  json["brightness"] = currentColor.brightness;
-  json["red"] = currentColor.red;
-  json["blue"] = currentColor.blue;
-  json["green"] = currentColor.green;
-
-  response.sendJsonObject(stream);
-}
-
-void ajaxSetColor(HttpRequest &request, HttpResponse &response) {
-  int red = request.getQueryParameter("red").toInt();
-  int green = request.getQueryParameter("green").toInt();
-  int blue = request.getQueryParameter("blue").toInt();
-
-  Serial.printf("\r\nRed: [%d] Green: [%d] Blue: [%d]", red, green, blue);
-  currentColor.red = (uint8_t) red;
-  currentColor.green = (uint8_t) green;
-  currentColor.blue = (uint8_t) blue;
-
-  currentColor.brightness = (uint8_t) request.getQueryParameter("brightness").toInt();
-
-  Serial.printf(
-    "\r\nNew values: r[%d] g[%d] b[%d] brightness[%d]",
-    currentColor.red,
-    currentColor.green,
-    currentColor.blue
-  );
-
-  updateLights();
-  ajaxGetColor(request, response);
-}
-
-void ajaxGetLength(HttpRequest &request, HttpResponse &response) {
-  JsonObjectStream* stream = new JsonObjectStream();
-  JsonObject& json = stream->getRoot();
-  json["length"] = LED_LENGTH;
-
-  response.sendJsonObject(stream);
-}
-
-void ajaxSetLength(HttpRequest &request, HttpResponse &response) {
-  LED_LENGTH = (size_t) request.getQueryParameter("length").toInt();
-  ajaxGetLength(request, response);
-}
-
-void onIndex(HttpRequest &request, HttpResponse &response) {
-  TemplateFileStream *tmpl = new TemplateFileStream("index.html");
-  auto &vars = tmpl->variables();
-  //vars["counter"] = String(counter);
-  response.sendTemplate(tmpl); // this template object will be deleted automatically
-}
-
-void startWebServer() {
-  server.listen(80);
-  server.addPath("/", onIndex);
-  server.addPath("/color/set", ajaxSetColor);
-  server.addPath("/color/get", ajaxGetColor);
-  server.addPath("/length/set", ajaxSetLength);
-  server.addPath("/length/get", ajaxGetLength);
-  server.setDefaultHandler(onFile);
-
-  Serial.println("\r\n=== WEB SERVER STARTED ===");
-  Serial.println(WifiStation.getIP());
-  Serial.println("==============================\r\n");
-}
+uint8_t brightness = 0xFF;
+UdpConnection udp(onUdpReceive);
 
 void init()
 {
   Serial.begin(SERIAL_BAUD_RATE);
   Serial.systemDebugOutput(true); // Allow debug print to serial
   SPI.begin();
-  updateLights();
 
-  //Ajax server
-  spiffs_mount(); // Mount file system, in order to work with files
+  //UDP server
   WifiStation.enable(true);
   WifiStation.config(WIFI_SSID, WIFI_PWD);
   WifiAccessPoint.enable(false);
 
-  WifiStation.waitConnection(startWebServer);
+  WifiStation.waitConnection(startTpmServer);
+}
+
+void startTpmServer() {
+  udp.listen(TPM2_CLIENT_PORT);
+}
+
+void onUdpReceive(UdpConnection& con, char *data, int size, IPAddress remoteIp, uint16_t remotePort) {
+  uint8_t *packet = (uint8_t *) data;
+  Serial.println("Got packet!");
+
+  if (tpm2_packet_is_tpm2(packet)) {
+    if (tpm2_packet_type(packet) == PACKET_DATA ) {
+      showColorBuffer(
+        tpm2_packet_payload(packet),
+        tpm2_packet_payload_size(packet),
+        brightness
+      );
+    }
+  }
+  con.sendStringTo(remoteIp, TPM2_ACK_PORT, TPM2_CLIENT_RESPONSE);
 }
--- include/apa102.cpp
+++ include/apa102.cpp
@@ -1,4 +1,5 @@
 #include <SmingCore/SPI.h>
+#include "apa102.hpp"
 
 void showColor(size_t len, uint8_t brightness, uint8_t b, uint8_t g, uint8_t r)
 {
@@ -25,4 +26,28 @@ void showColor(size_t len, uint8_t brightness, uint8_t b, uint8_t g, uint8_t r)
 
   sprintf(message, "%d lights sent", count);
   Serial.println(message);
+}
+
+void showColorBuffer(uint8_t *buffer, size_t len, uint8_t brightness) {
+  int k, l;
+  int lights = len / 3;
+
+  //Preamble
+  for (k = 0; k < 4; k++) {
+    SPI.transfer(0);
+  }
+
+  // Add global brightness to beginning of the triplets for color
+  for (k = 0; k < lights; k += 3) {
+    SPI.transfer(brightness);
+    for (l = 0; l < 3; l++) {
+      SPI.transfer(buffer[k + l]);
+    }
+  }
+
+  // Number of bytes to push the data all out
+  l = (lights + 14) / 16;
+  for (k = 0; k < l; k++) {
+    SPI.transfer(1);
+  }
 }
